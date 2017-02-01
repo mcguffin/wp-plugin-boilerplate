cecf02827dab68056a3161fbea1adb7626697a43
mcguffin <joern@flyingletters.com>
2017-02-01 18:22:56 +0100

    plugin settings UI

--- templates/v2/include/__plugin_namespace__/Settings/Settings.php
+++ templates/v2/include/__plugin_namespace__/Settings/Settings.php
@@ -21,4 +21,39 @@ abstract class Settings extends Core\Singleton {
 
 	abstract function register_settings();
 
+
+	/**
+	 *	Print a checkbox
+	 *
+	 *	@param $args	array( $option_name, $label )
+	 */
+	public function checkbox_ui( $args ) {
+		@list( $option_name, $label, $description ) = $args;
+		
+		$option_value = get_option( $option_name );
+		
+		?><label>
+			<input type="hidden" name="<?php echo $option_name ?>" value="0" />
+			<input type="checkbox" <?php checked( boolval( $option_value ), true, true ); ?> name="<?php echo $option_name ?>" value="1" />
+			<?php echo $label ?>
+		</label>
+		<?php 
+			if ( ! empty( $description ) ) {
+				printf( '<p class="description">%s</p>', $description );
+			}
+		?>
+		<?php
+
+	}
+
+	/**
+	 *	Sanitize checkbox input
+	 *
+	 *	@param $value
+	 *	@return boolean
+	 */
+	public function sanitize_checkbox( $value ) {
+		return boolval( $value );
+	}
+
 }
