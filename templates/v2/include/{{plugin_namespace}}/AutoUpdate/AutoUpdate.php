f9d14584f5282391fc7aa285fc7cc51b6edf5724
mcguffin <joern@flyingletters.com>
2017-12-03 16:43:14 +0100

    autoupdate

--- templates/v2/include/{{plugin_namespace}}/AutoUpdate/AutoUpdate.php
+++ templates/v2/include/{{plugin_namespace}}/AutoUpdate/AutoUpdate.php
@@ -11,6 +11,12 @@ use {{plugin_namespace}}\Core;
 
 abstract class AutoUpdate extends Core\Singleton {
 
+
+	/**
+	 *	@var array Current release info
+	 */
+	protected $release_info = null;
+
 	/**
 	 *	@inheritdoc
 	 */
@@ -21,6 +27,8 @@ abstract class AutoUpdate extends Core\Singleton {
 		add_filter( 'upgrader_source_selection', array( $this, 'source_selection' ), 10, 4 );
 
 		add_action( 'upgrader_process_complete', array( $this, 'upgrade_completed' ), 10, 2 );
+
+		add_filter( 'plugins_api', array( $this, 'plugins_api' ), 10, 3 );
 
 	}
 
@@ -45,6 +53,65 @@ abstract class AutoUpdate extends Core\Singleton {
 		}
 	}
 
+
+	/**
+	 *	@filter plugin_api
+	 */
+	public function plugins_api( $res, $action, $args ) {
+		$slug = basename({{plugin_slug_upper}}_DIRECTORY);
+		if ( $_REQUEST['plugin'] === $slug ) {
+			/*
+
+			'Name'        => 'Plugin Name',
+			'PluginURI'   => 'Plugin URI',
+			'Version'     => 'Version',
+			'Description' => 'Description',
+			'Author'      => 'Author',
+			'AuthorURI'   => 'Author URI',
+			'TextDomain'  => 'Text Domain',
+			'DomainPath'  => 'Domain Path',
+			'Network'     => 'Network',
+
+
+			*/
+
+
+			$plugin_info	= get_plugin_data( {{plugin_slug_upper}}_FILE );
+			$release_info	= $this->get_release_info();
+
+			$plugin_api = array(
+				'name'						=> $plugin_info['Name'],
+				'slug'						=> $slug,
+//				'version'					=> $release_info, // release
+				'author'					=> $plugin_info['Author'],
+				'author_profile'			=> $plugin_info['AuthorURI'],
+//				'contributors'				=> array(),
+//				'requires'					=> '',
+//				'tested'					=> '',
+//				'requires_php'				=> '',
+				'compatibility'				=> array(),
+				'rating'					=> 0,
+				'num_ratings'				=> 0,
+				'support_threads'			=> 0,
+				'support_threads_resolved'	=> 0,
+//				'active_installs'			=> 0,
+//				'last_updated'				=> '2017-11-22 2:41pm GMT', // format!
+//				'added'						=> '2017-11-22', // format!
+				'homepage'					=> $plugin_info['PluginURI'],
+				'sections'					=> $this->get_plugin_sections(),
+//				'download_link'	=> '',
+//				'screenshots'				=> array(),
+//				'tags'						=> array(),
+//				'versions'					=> array(),	// releases?
+//				'donate_link'				=> '',
+				'banners'					=> $this->get_plugin_banners(),
+				'external'					=> true,
+			) + $release_info;
+
+			return (object) $plugin_api;
+		}
+		return $res;
+	}
 	/**
 	 *	Will make sure that the downloaded directory name and our plugins dirname are the same.
 	 *	@filter upgrader_source_selection
@@ -99,18 +166,19 @@ abstract class AutoUpdate extends Core\Singleton {
 			$plugin_info	= get_plugin_data( {{plugin_slug_upper}}_FILE );
 
 			if ( version_compare( $release_info['version'], $plugin_info['Version'] , '>' ) ) {
+
 				$transient->response[ $plugin ] = (object) array(
 					'id'			=> $release_info['id'],
 					'slug'			=> $slug,
 					'plugin'		=> $plugin,
 					'new_version'	=> $release_info['version'],
 					'url'			=> $plugin_info['PluginURI'],
-					'package'		=> $release_info['download_url'],
+					'package'		=> $release_info['download_link'],
 					'icons'			=> array(),
 					'banners'		=> array(),
 					'banners_rtl'	=> array(),
-					//'tested'		=> '',
-					'compatibility'	=> array(),
+					'tested'		=> $release_info['tested'],
+					'compatibility'	=> (object) array(),
 				);
 				if ( isset( $transient->no_update ) && isset( $transient->no_update[$plugin] ) ) {
 					unset( $transient->no_update[$plugin] );
@@ -130,7 +198,71 @@ abstract class AutoUpdate extends Core\Singleton {
 	 *		'download_url'	=> 'https://...'
 	 *	)
 	 */
-	abstract function get_release_info();
+	protected function get_release_info() {
 
+		if ( is_null( $this->release_info ) ) {
+			$this->release_info = $this->get_remote_release_info();
+		}
+
+		return $this->release_info;
+	}
+
+	/**
+	 *	Should fetch info for current release and return it
+	 *
+	 *	@return array(
+	 *		'id'			=> '...'
+	 *		'version'		=> '...'
+	 *		'download_url'	=> 'https://...'
+	 *	)
+	 */
+	abstract function get_remote_release_info();
+
+	/**
+	 *	Should return plugin page sections
+	 *
+	 *	@return array(
+	 *		'section title'		=> '<Section html>',
+	 *		'another section'	=> '...',
+	 *		'...'
+	 *	)
+	 */
+	protected function get_plugin_sections() {
+		return array();
+	}
+
+	/**
+	 *	Return plugin banners
+	 *
+	 *	@return array(
+	 *		'low'		=> '<banner URL 772x250px>',
+	 *		'high'		=> '<banner URL 1544x500px>',
+	 *	)
+	 */
+	protected function get_plugin_banners() {
+		return array();
+	}
+
+	/**
+	 *	Like WP‘s get_file_data() but with a String
+	 *
+	 *	@param	string	$data
+	 *	@param	array	$info
+	 *	@return array
+	 */
+	protected function extract_info( $data, $info ) {
+
+		// normalize LFs
+		$data = str_replace( "\r", "\n", $data );
+
+		foreach ( $info as $field => $regex ) {
+			if ( preg_match( '/^[ \t\/*#@]*' . preg_quote( $regex, '/' ) . ':(.*)$/mi', $data, $match ) && $match[1] ) {
+				$info[ $field ] = _cleanup_header_comment( $match[1] );
+			} else {
+				$info[ $field ] = '';
+			}
+		}
+		return array_filter( $info );
+	}
 
 }
