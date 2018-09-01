3e7393b9a0f2b487dffd8f250003ddfdaaf07bc3
mcguffin <joern@flyingletters.com>
2018-09-01 12:09:56 +0200

    Support afragen/github-updater

--- templates/v2/README.md
+++ templates/v2/README.md
@@ -2,3 +2,24 @@
 ===============
 
 #### Developer info here. ####
+
+
+Installation
+------------
+
+### Production (Stand-Alone)
+ - Head over to [releases](../../releases)
+ - Download '{{wp_plugin_slug}}.zip'
+ - Upload and activate it like any other WordPress plugin
+ - AutoUpdate will run as long as the plugin is active
+
+### Production (using Github Updater – recommended for Multisite)
+ - Install [Andy Fragen's GitHub Updater](https://github.com/afragen/github-updater) first.
+ - Go to Settings / GitHub Updater / Install Plugin. Enter `{{modules.git.github_repo}}` as Plugin-URI.
+
+### Development
+ - cd into your plugin directory
+ - $ `git clone git@github.com:{{modules.git.github_repo}}.git`
+ - $ `cd {{wp_plugin_slug}}`{{#modules.gulp}}
+ - $ `npm install`
+ - $ `gulp`{{/modules.gulp}}
--- templates/v2/index.php
+++ templates/v2/index.php
@@ -10,12 +10,14 @@ Author URI: {{plugin_author_uri}}
 License: GPL3
 {{#modules.git}}
 Github Repository: {{modules.git.github_repo}}
+GitHub Plugin URI: {{modules.git.github_repo}}
+Release Asset: true
 {{/modules.git}}
 Text Domain: {{wp_plugin_slug}}
 Domain Path: /languages/
 */
 
-/*  Copyright {{this_year}}  {{plugin_author}}
+/*  Copyright {{this_year}} {{plugin_author}}
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
@@ -83,7 +85,17 @@ if ( is_admin() || defined( 'DOING_AJAX' ) ) {
 {{#modules.autoupdate}}
 	// don't WP-Update actual repos!
 	if ( ! file_exists( {{plugin_slug_upper}}_DIRECTORY . '/.git/' ) ) {
-		AutoUpdate\AutoUpdateGithub::instance()->init( __FILE__ );
+
+		// Not a git. Check if https://github.com/afragen/github-updater is active
+		$active_plugins = get_option('active_plugins');
+		if ( $sitewide_plugins = get_site_option('active_sitewide_plugins') ) {
+			$active_plugins = array_merge( $active_plugins, array_keys( $sitewide_plugins ) );
+		}
+
+		if ( ! in_array( 'github-updater/github-updater.php', $active_plugins ) ) {
+			// not github updater. Init our our own...
+			AutoUpdate\AutoUpdateGithub::instance()->init( __FILE__ );
+		}
 	}
 {{/modules.autoupdate}}
 
