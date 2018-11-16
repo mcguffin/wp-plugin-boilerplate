11eacc572b40456fda0f08ffc0d49353e6003d38
mcguffin <joern@flyingletters.com>
2018-11-16 09:09:09 +0100

    acf fieldgroup json

--- wp_plugin/modules/compat.py
+++ wp_plugin/modules/compat.py
@@ -8,3 +8,4 @@ class compat(m.plugin_module):
 		self.add_template('include/{{plugin_namespace}}/Compat/Polylang.php')
 		self.add_template('include/{{plugin_namespace}}/Compat/RegenerateThumbnails.php')
 		self.add_template('include/{{plugin_namespace}}/Compat/WPMU.php')
+		self.add_template('include/acf-json/group_PREFIX_GROUPNAME.json.tpl')
